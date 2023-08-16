fa15a73c63ba632c33bad3c8a99d5eb52e3d2c78
justjust1n1126 <75755009+justjust1n1126@users.noreply.github.com>
2023-08-16 12:20:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const player = document.querySelector(".player");
+    const enemy = document.querySelector(".enemy");
+    const boss = document.querySelector(".boss");
+    const minions = document.querySelectorAll(".minion");
+    const powerUp = document.querySelector(".power-up");
+    const gameOver = document.querySelector(".game-over");
+
+    let isGameOver = false;
+    let isTripleShotActive = false;
+    let isInvincible = false;
+    let isNukeActive = false;
+    let isBossActive = false;
+    let stage = 1;
+    let enemyHealth = 1;
+    let enemySpeed = 10;
+    let minionSpeed = 5;
+
+    // ... previous code ...
+
+    // The full JavaScript code from the previous responses should go here.
+
+    // ... previous code ...
+});
+
